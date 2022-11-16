31fa7152a135eb0a08a09db6ff1361c08f0e9047
Yellow10041 <yellowvatral@gmail.com>
2022-11-16 05:34:23 +0200

    Update Technology

--- src/components/Pages/Technology/elements/Cards/Cards.jsx
+++ src/components/Pages/Technology/elements/Cards/Cards.jsx
@@ -4,12 +4,16 @@ import { useState, useRef } from 'react'
 
 import { gsap } from "gsap";
 
+import IMG1 from '../../../../../assets/img/Technology/img1.png';
+
 //Elements
 
 function Cards () {
 
     const [cardItemActive, setCardItemActive] = useState(null);
     const refCards = useRef([]);
+    const refCardsImg = useRef([]);
+    const refCardsImgContainer = useRef();
 
     const dataCards = [
         {
@@ -28,11 +32,23 @@ function Cards () {
 
     const CardActiveChange = (e) => {
         const index = e.currentTarget.dataset.index;
-
+        setCardItemActive(index);
         let tl = gsap.timeline();
-        tl.to(refCards.current, 
-            { maxWidth: 800, duration : 1, ease:"power2.inOut" }
-        );
+
+        if(index == cardItemActive) {
+            tl.to(refCards.current[index], 
+                { maxWidth: 800, duration : 1, ease:"power2.inOut" }
+            ).to(refCardsImg.current[index], 
+                { opacity : 1, width: '100%', height: '100%', duration : 1, ease:"expo.inOut" }
+            ).reverse(1);
+        } else {
+            tl.to(refCards.current[index], 
+                { maxWidth: 800, duration : 1, ease:"power2.inOut" }
+            ).to(refCardsImg.current[index], 
+                { opacity : 1, width: '100%', height: '100%', duration : 1, ease:"expo.inOut" }
+            );
+        }
+        
     }
 
     return (
@@ -44,19 +60,23 @@ function Cards () {
                     <div className={style.Cards_des}>Sonr’s architecture is designed to ensure a reliable and secure end-to-end experience, compliant with key protocols and specifications from leading orgs, like WC3 and IPLD.</div>
                 </div>
                 <div className={style.Cards_content}>
-                    {dataCards.map((item, index) => (
-                        <div className={style.Cards_item} ref={refCards} data-index={index} onClick={CardActiveChange}>
-                            <div className={style.Cards_item_box}>
-                                <div className={style.Cards_item_num}>0{index+1}</div>
-                                <div className={style.Cards_item_title}>{item.title}</div>
-                                <div className={style.Cards_item_subtitle}>{item.des}</div>
-                                <div className={style.Cards_item_border}>
-                                    <div className={style.Cards_item_border_item}></div>
+                    <div className={style.Cards_content_container}>
+                        {dataCards.map((item, index) => (
+                            <div className={style.Cards_item} ref={(el) => (refCards.current[index] = el)} data-index={index} onClick={CardActiveChange}>
+                                <div className={style.Cards_item_box}>
+                                    <div className={style.Cards_item_num}>0{index+1}</div>
+                                    <div className={style.Cards_item_title}>{item.title}</div>
+                                    <div className={style.Cards_item_subtitle}>{item.des}</div>
+                                    <div className={style.Cards_item_border}>
+                                        <div className={style.Cards_item_border_item}></div>
+                                    </div>
+                                </div>
+                                <div className={style.Cards_item_img_container} ref={refCardsImgContainer}>
+                                    <img className={style.Cards_item_img} ref={(el) => (refCardsImg.current[index] = el)} src={IMG1} alt="" />
                                 </div>
                             </div>
-                            <img src="" alt="" />
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>

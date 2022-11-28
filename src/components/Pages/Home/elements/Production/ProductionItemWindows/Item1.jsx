1db755da3d9c634eef9a61cdbbb0d16a016280da
Yellow10041 <yellowvatral@gmail.com>
2022-11-29 00:02:28 +0200

    Update Home

--- src/components/Pages/Home/elements/Production/ProductionItemWindows/Item1.jsx
+++ src/components/Pages/Home/elements/Production/ProductionItemWindows/Item1.jsx
@@ -68,6 +68,25 @@ function Item1() {
                     <path d="M113.875 124C116.567 124 118.75 121.817 118.75 119.125C118.75 116.433 116.567 114.25 113.875 114.25C111.182 114.25 109 116.433 109 119.125C109 121.817 111.182 124 113.875 124Z" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M91.3359 135.375C92.0672 132.531 94.6672 130.418 97.7384 130.435L103.312 130.451C107.57 130.467 111.193 127.737 112.526 123.935" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
+                <svg className={`${styleG.svg} ${styleG.svg_1_mob}`} width="175" height="199" viewBox="0 0 175 199" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 1L20.466 23H50" stroke="#40414C"/>
+                    <path d="M42.9176 94.5824L62.5 94.4268L82.0824 94.5824L92.0083 111.463L101.665 128.5L92.0083 145.537L82.0824 162.418L62.5 162.573L42.9176 162.418L32.9917 145.537L23.3353 128.5L32.9917 111.463L42.9176 94.5824Z" fill="#131313" stroke="#40414C"/>
+                    <path d="M56.8867 132.477V126.5" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M57.1 138.456C58.6327 138.456 59.8753 137.213 59.8753 135.68C59.8753 134.148 58.6327 132.905 57.1 132.905C55.5672 132.905 54.3247 134.148 54.3247 135.68C54.3247 137.213 55.5672 138.456 57.1 138.456Z" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M56.8865 126.5C58.3014 126.5 59.4483 125.353 59.4483 123.939C59.4483 122.524 58.3014 121.377 56.8865 121.377C55.4717 121.377 54.3247 122.524 54.3247 123.939C54.3247 125.353 55.4717 126.5 56.8865 126.5Z" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M68.8416 126.5C70.2564 126.5 71.4034 125.353 71.4034 123.939C71.4034 122.524 70.2564 121.377 68.8416 121.377C67.4267 121.377 66.2798 122.524 66.2798 123.939C66.2798 125.353 67.4267 126.5 68.8416 126.5Z" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M56.9976 132.477C57.3818 130.983 58.7481 129.873 60.3621 129.881L63.2911 129.89C65.5284 129.898 67.4326 128.464 68.1329 126.466" stroke="#E2B7F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M108.823 140.177L122 140.072L135.177 140.177L141.856 151.536L148.354 163L141.856 174.464L135.177 185.823L122 185.928L108.823 185.823L102.144 174.464L95.6462 163L102.144 151.536L108.823 140.177Z" fill="#131313" stroke="#40414C"/>
+                    <path d="M116.364 159.815L113.446 162.732L116.364 165.65" stroke="#7265FF" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M128.034 159.815L130.951 162.732L128.034 165.65" stroke="#7265FF" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M123.658 159.333L120.74 166.131" stroke="#7265FF" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M149 163H170.546V190.701" stroke="#40414C"/>
+                    <circle cx="170.546" cy="194.512" r="3.5" stroke="#40414C"/>
+                    <circle cx="4" cy="98" r="3.5" stroke="#40414C"/>
+                    <path d="M24.0809 128.294H3.90332V101.618" stroke="#40414C"/>
+                    <path d="M90 108.173H121.121L144.377 75" stroke="#40414C"/>
+                </svg>
+
             </div>
         </div>
     )
